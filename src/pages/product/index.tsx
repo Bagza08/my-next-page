a56fab502234925ca89fb4770d2509bcc032ba06
Bagza <bagzajayawiguna639@gmail.com>
2024-10-19 14:44:20 +0700

    update

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+type productType = {
+  id: number;
+  name: string;
+  price: number;
+  size: string;
+  description: string;
+};
 
 function Product() {
+  const [isLogin] = useState(true);
+  const [product, setProduct] = useState([]);
+
+  const route = useRouter();
+
+  useEffect(() => {
+    if (!isLogin) {
+      route.push("/auth/login");
+    }
+  }, [isLogin, route]);
+
+  useEffect(() => {
+    fetch("/api/product")
+      .then((res) => res.json())
+      .then((response) => setProduct(response?.data));
+  }, []);
+
   return (
-    <div>Product</div>
-  )
+    <>
+      <h1>Product</h1>
+      <div>
+        {product?.length > 0 &&
+          product?.map((item: productType) => (
+            <div key={item?.id}>{item?.name}</div>
+          ))}
+      </div>
+    </>
+  );
 }
 
-export default Product
+export default Product;
