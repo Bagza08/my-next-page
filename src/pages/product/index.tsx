1913c0cf0c6ab1ef834589cecaac691f1b1a8488
Bagza <bagzajayawiguna639@gmail.com>
2024-12-03 19:50:03 +0700

    page-12-middleware

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,21 +1,9 @@
 import { fetcher } from "@/lib/swr/fetcher";
 import ProductViews from "@/views/product";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import useSWR from "swr";
 
 function Product() {
-  const [isLogin] = useState(true);
-
   const { data } = useSWR("/api/product", fetcher);
-
-  const route = useRouter();
-
-  useEffect(() => {
-    if (!isLogin) {
-      route.push("/auth/login");
-    }
-  }, [isLogin, route]);
 
   // useEffect(() => {
   //   fetch("/api/product")
