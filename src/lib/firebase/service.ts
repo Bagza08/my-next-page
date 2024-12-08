5820b0d794176736cd7acdd43f383a31265b36c0
Bagza <bagzajayawiguna639@gmail.com>
2024-12-08 13:48:08 +0700

    14. auth register

--- src/lib/firebase/service.ts
+++ src/lib/firebase/service.ts
@@ -1,11 +1,15 @@
 import {
+  addDoc,
   collection,
   doc,
   getDoc,
   getDocs,
   getFirestore,
+  query,
+  where,
 } from "firebase/firestore";
 import app from "./init";
+import bcyrp from "bcrypt";
 
 const firestore = getFirestore(app);
 
@@ -27,4 +31,35 @@ export const retrieveDataById = async (collectionName: string, id: string) => {
   const data = snapshot.data();
 
   return data;
+};
+
+export const signUp = async (
+  userdata: {
+    email: string;
+    fullname: string;
+    password: string;
+    role?: string;
+  },
+  callback: (arg0: boolean, arg1: string) => void
+) => {
+  const q = query(
+    collection(firestore, "users"),
+    where("email", "==", userdata.email)
+  );
+
+  const snapshot = await getDocs(q);
+  const data = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  if (data.length > 0) {
+    callback(false, "Email already exist");
+  } else {
+    userdata.password = await bcyrp.hash(userdata.password, 10);
+    userdata.role = "member";
+    await addDoc(collection(firestore, "users"), userdata).then(() => {
+      callback(true, "register success");
+    }).catch((err) => callback(false, err.message));
+  }
 };
--- src/views/auth/register/index.tsx
+++ src/views/auth/register/index.tsx
@@ -1,17 +1,95 @@
 import { useRouter } from "next/router";
 import style from "./Register.module.scss";
+import { useState } from "react";
 
 function RegisterPage() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
   const router = useRouter();
   const handlelogin = () => {
     router.push("/auth/login");
   };
 
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    const data = {
+      email: event.target.email.value,
+      fullname: event.target.fullname.value,
+      password: event.target.password.value,
+    };
+
+    const result = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (result.status === 200) {
+      event.target.reset();
+      setLoading(false);
+      router.push("/auth/login");
+    } else {
+      setLoading(false);
+      setError(result.status === 400 ? "Email already exist" : "");
+    }
+  };
+
   return (
     <div className={style.container}>
       <div className="text-3xl mb-3">Register page</div>
-      <p onClick={handlelogin} style={{ cursor: "pointer", fontSize: "20px" }}>
-        login disini!
+      <div className={style.error}>{error}</div>
+      <form onSubmit={handleSubmit} className={style.container}>
+        <div className="flex flex-col mb-3">
+          <label htmlFor="fullname" className="text-xl text-white mb-2">
+            Fullname
+          </label>
+          <input
+            type="text"
+            name="fullname"
+            id="fullname"
+            placeholder="masukan fullname"
+          />
+        </div>
+        <div className="flex flex-col mb-3">
+          <label htmlFor="email" className="text-xl text-white mb-2">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="masukan email"
+          />
+        </div>
+        <div className="flex flex-col mb-3">
+          <label htmlFor="password" className="text-xl text-white mb-2">
+            password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="masukan password"
+          />
+        </div>
+        <button type="submit" className="bg-white text-black px-10 mt-1" disabled={loading}>
+          {loading ? "loading..." : "register"}
+        </button>
+      </form>
+      <p
+        onClick={handlelogin}
+        style={{
+          cursor: "pointer",
+          fontSize: "15px",
+          marginTop: "20px",
+          color: "orange",
+        }}
+      >
+        sudah punya akun? login disini!
       </p>
     </div>
   );
